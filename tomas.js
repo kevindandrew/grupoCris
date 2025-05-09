015839cc56d369c62cd53271ea2b164fa595b34f
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-09 13:59:59 -0300

    Se añadió la función para filtrar y extraer a todos los personajes

--- tomas.js
+++ tomas.js
@@ -12,4 +12,38 @@ async function extractAllCharaters() {
   }
 }
 
-export { extractAllCharaters };
+//FUNCIÓN PARA DEVOLVER A TODOS LOS PERSONAJES FILTRADOS POR ESPECIE O ESTADO
+
+async function filterAndExtractCharacters(
+  filter_status = "",
+  filter_species = ""
+) {
+  /*
+  PARÁMETROS:
+  filter_status: "alive" - "dead"
+  filter_species: "human" - "alien"
+  */
+  try {
+    if (filter_status !== "" && filter_species === "") {
+      const { data } = await axios.get(
+        `https://rickandmortyapi.com/api/character/?status=${filter_status}`
+      );
+      return data.results;
+    } else if (filter_species !== "" && filter_status === "") {
+      const { data } = await axios.get(
+        `https://rickandmortyapi.com/api/character/?species=${filter_species}`
+      );
+      return data.results;
+    } else if (filter_status !== "" && filter_species !== "") {
+      const { data } = await axios.get(
+        `https://rickandmortyapi.com/api/character/?species=${filter_species}&status=${filter_status}`
+      );
+      return data.results;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { extractAllCharaters, filterAndExtractCharacters };
